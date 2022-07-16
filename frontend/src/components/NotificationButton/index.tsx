53a4aaf517d03d00dbd973aa5650c7c91882a7dd
LaelMartinez <96741102+LaelMartinez@users.noreply.github.com>
2022-07-16 17:33:20 -0300

    Notificação

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,9 +1,25 @@
+import axios from 'axios';
+import { toast } from 'react-toastify';
 import icon from '../../assets/img/notification-icon.svg';
+import { BASE_URL } from '../../utils/request';
 import './style.css';
 
-function NotificationButton() {
+type Props = {
+    saleId: number; 
+}
+
+function handleClick(Id :number){
+    axios.get(`${BASE_URL}/sales/${Id}/notification`)
+        .then(response => {
+            toast.info("SMS Enviado com socesso!");
+
+            }
+        )
+}
+
+function NotificationButton({saleId} : Props) {
     return (
-        <div className="dsmeta-red-btn">
+        <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
             <img src={icon} alt="Notificar"/>
         </div>
 
--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -2,7 +2,10 @@ import NotificationButton from "../NotificationButton";
 import "./styles.css";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { BASE_URL } from "../../utils/request";
+import { Sale } from "../../models/sale";
 
 function SalesCard() {
 
@@ -11,7 +14,21 @@ function SalesCard() {
 
     const [minDate, setMinDate] = useState(min);
     const [maxDate, setMaxDate] = useState(max);
-    
+
+    const [sales, setSales] = useState<Sale[]>([]);
+
+
+    useEffect(() => {
+        const dmin = minDate.toISOString().slice(0, 10);
+        const dmax = maxDate.toISOString().slice(0, 10);
+        axios.get(`${BASE_URL}/sales`)
+//        axios.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`)
+            .then(response => {
+                setSales(response.data.content);
+            });
+    }, [minDate, maxDate]);
+//}, []);
+
     return (
         <div className="dsmeta-card">
             <h2 className="dsmeta-sales-title">Vendas</h2>
@@ -19,7 +36,7 @@ function SalesCard() {
                 <div className="dsmeta-form-control-container">
                     <DatePicker
                         selected={minDate}
-                        onChange={(date: Date) => {setMinDate(date) }}
+                        onChange={(date: Date) => { setMinDate(date) }}
                         className="dsmeta-form-control"
                         dateFormat="dd/MM/yyyy"
                     />
@@ -27,7 +44,7 @@ function SalesCard() {
                 <div className="dsmeta-form-control-container">
                     <DatePicker
                         selected={maxDate}
-                        onChange={(date: Date) => { setMaxDate(date)}}
+                        onChange={(date: Date) => { setMaxDate(date) }}
                         className="dsmeta-form-control"
                         dateFormat="dd/MM/yyyy"
                     />
@@ -62,46 +79,23 @@ function SalesCard() {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td className="show992">#341</td>
-                            <td className="show576">02/11/1980</td>
-                            <td>Anakin</td>
-                            <td className="show992">15</td>
-                            <td className="show992">11</td>
-                            <td>55300.00</td>
-                            <td>
-                                <div className="dsmeta-red-btn-container">
-                                    <NotificationButton></NotificationButton>
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="show992">2</td>
-                            <td className="show576">02/11/1980</td>
-                            <td>Anakin</td>
-                            <td className="show992">Visitas 2</td>
-                            <td className="show992">Vendas 2</td>
-                            <td>55300.00</td>
-                            <td>
-                                <div className="dsmeta-red-btn-container">
-                                    <NotificationButton></NotificationButton>
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="show992">3</td>
-                            <td className="show576">02/11/1980</td>
-                            <td>Anakin</td>
-                            <td className="show992">Visitas 3</td>
-                            <td className="show992">Vendas 3</td>
-                            <td>55300.00</td>
-                            <td>
-                                <div className="dsmeta-red-btn-container">
-                                    <NotificationButton></NotificationButton>
-                                </div>
-                            </td>
-                        </tr>
-
+                        {sales.map(sale => {
+                            return (
+                                <tr key={sale.id}>
+                                    <td className="show992">{sale.id}</td>
+                                    <td className="show576">{new Date(sale.date).toLocaleDateString()}</td>
+                                    <td>{sale.sellerName}</td>
+                                    <td className="show992">{sale.visited}</td>
+                                    <td className="show992">{sale.deals}</td>
+                                    <td>R$ {sale.amount.toFixed(2)}</td>
+                                    <td>
+                                        <div className="dsmeta-red-btn-container">
+                                            <NotificationButton saleId={sale.id}></NotificationButton>
+                                        </div>
+                                    </td>
+                                </tr>
+                            )
+                        })}
                     </tbody>
                 </table>
             </div>
